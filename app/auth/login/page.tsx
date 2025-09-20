2cb05c0bd4a02f87909f27eb18fd635a0ee5c9a7
TomoyaOgashiwa <tomoya07oga@gmail.com>
2025-09-20 12:26:36 -0700

    Add AuthLayout component and enhance login functionality
    - Introduced a new `AuthLayout` component to structure authentication pages.
    - Updated login page to include a "Login with Google" button and improved layout.
    - Refactored Supabase client creation to use `createServerClient` for consistency across authentication actions.
    - Added `loginWithGoogle` function to facilitate Google OAuth login in the login action module.
    - Ensured proper error handling and redirection after authentication attempts.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,18 +1,24 @@
-import { login, signup } from "./action/login";
+import { login, loginWithGoogle, signup } from "./action/login";
 
 export default function LoginPage() {
   return (
-    <form className="flex flex-col gap-4">
-      <label className="flex gap-2" htmlFor="email">
-        Email:
-        <input id="email" name="email" type="email" required />
-      </label>
-      <label className="flex gap-2" htmlFor="password">
-        Password:
-        <input id="password" name="password" type="password" required />
-      </label>
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex flex-col gap-4">
+      <h2>Login</h2>
+      <form className="flex flex-col gap-4">
+        <label className="flex gap-2" htmlFor="email">
+          Email:
+          <input id="email" name="email" type="email" required />
+        </label>
+        <label className="flex gap-2" htmlFor="password">
+          Password:
+          <input id="password" name="password" type="password" required />
+        </label>
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+      <button onClick={loginWithGoogle} type="button">
+        Login with Google
+      </button>
+    </div>
   );
 }
--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,9 +1,9 @@
 import { redirect } from "next/navigation";
 
-import { createClient } from "@/libs/supabase/server";
+import { createServerClient } from "@/libs/supabase/server";
 
 export default async function PrivatePage() {
-  const supabase = await createClient();
+  const supabase = await createServerClient();
 
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
