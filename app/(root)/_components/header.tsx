35c5ddf57d44f56fe2413d59349764eb16ef6538
TomoyaOgashiwa <tomoya07oga@gmail.com>
2025-09-22 16:12:33 -0700

    style: login form and add shadcn/ui

---
+++ app/(root)/_components/header.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { User } from "@supabase/supabase-js";
+import { logout } from "../action/logout";
+
+interface Props {
+  user: User;
+}
+
+export default function Header({ user }: Props) {
+  return (
+    <header className="flex justify-between items-center p-4 sticky bg-[inherit] border-b border-gray-50 shadow-2xl">
+      <h1>Job Apply Flow</h1>
+      <div className="flex items-center gap-4">
+        <p>{user.email}</p>
+        <button type="button" className="cursor-pointer" onClick={logout}>
+          Sign Out
+        </button>
+      </div>
+    </header>
+  );
+}
